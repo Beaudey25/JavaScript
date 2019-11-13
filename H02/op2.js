2dd09eb8547bd151a810f8f97045407385b618a5
beaudey <2110836@student.rocvf.nl>
2019-11-13 11:23:07 +0100

    .

--- H02/op2.js
+++ H02/op2.js
@@ -1,5 +1,11 @@
-var a =10;
-
-for (var i = 0; i < 10  ; i++) {
-    console.log("De waarde van i is " + i);
-}
+blok1.style.backgroundColor= "aqua";
+blok2.style.backgroundColor= "green";
+blok3.style.backgroundColor= "yellow";
+blok4.style.backgroundColor= "red";
+blok2.style.marginTop= "20";
+blok3.style.marginTop= "50";
+blok1.style.paddingLeft= "30";
+blok1.style.paddingTop= "20";
+blok2.style.paddingLeft= "30";
+blok3.style.paddingLeft= "62";
+document.getElementById("tekst").style.fontFamily = "Saira Extra Condensed";
